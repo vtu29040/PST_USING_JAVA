0482a86c10c598cd29adda2fd381440be41e0839
Adesh Prasad <adeshprasad010@gmail.com>
2026-09-21 11:23:49 +0530

    Implement Week 7 Codes and outputs

---
+++ Week7 Codes/Week7_Task6_GradingStudents.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Week7_Task6_GradingStudents {
+    static int gradeOf(int grade) {
+        if (grade < 38) {
+            return grade;
+        }
+        int nextMultiple = grade + (5 - grade % 5) % 5;
+        if (nextMultiple == grade) {
+            return grade;
+        }
+        return (nextMultiple - grade) < 3 ? nextMultiple : grade;
+    }
+
+    public static void main(String[] args) {
+        int[] grades = { 73, 67, 38, 33 };
+        int[] result = Arrays.stream(grades).map(Week7_Task6_GradingStudents::gradeOf).toArray();
+        System.out.println("Input: " + Arrays.toString(grades));
+        System.out.println("Output: " + Arrays.toString(result));
+    }
+}
