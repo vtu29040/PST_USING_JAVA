5aaa82e70730bd91a7db0bc6328f7b6f4258907f
Adesh Prasad <adeshprasad010@gmail.com>
2026-09-24 09:23:09 +0530

    Implement Week 9 codes and outputs

---
+++ Week9 Codes/Week9_Task5_JavaExceptionHandlingTryCatch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Week9_Task5_JavaExceptionHandlingTryCatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            try {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                System.out.println(x / y);
+            } catch (ArithmeticException e) {
+                System.out.println("Exception caught: " + e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println("Exception caught: " + e);
+                sc.next();
+            }
+        }
+    }
+}
